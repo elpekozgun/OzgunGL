d1249bcd3b9bfe8db185693e2baf87526e850d1f
elpekozgun <elpekozgun@gmail.com>
2018-04-29 12:57:46 +0300

    Working window with polled events using GLAD

--- OzgunGL/src/MainWindow.cpp
+++ OzgunGL/src/MainWindow.cpp
@@ -1,15 +1,15 @@
 #include <string>
 //#include <GL/glew.h>
 //#include <GL/GLU.h>
-#include<glad.h>
+#include<glad\glad.h>
 #include <GLFW/glfw3.h>
 #include "Macros.h"
 
-using namespace std;
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
+void ProcessInput(GLFWwindow* window);
+int Render(GLFWwindow* window);
 
-static GLFWwindow* window;
-
-static int StartGL()
+static int StartGL(GLFWwindow* window)
 {
 	if (!glfwInit())
 		CLOSEWIN();
@@ -28,16 +28,15 @@ static int StartGL()
 
 	glfwMakeContextCurrent(window);
 
-	//if (glewInit() != GLEW_OK)
-	//	LOG("Glew Error");
+	//if (GL_COMPILE_STATUS)
+		//LOG("Glew Error");
 	//LOG(glGetString(GL_VERSION));
 
 	glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
 
 	return 0;
 }
-
-static int UpdateGL()
+static int UpdateGL(GLFWwindow* window)
 {
 	while (!glfwWindowShouldClose(window))
 	{
@@ -48,9 +47,70 @@ static int UpdateGL()
 	return 0;
 }
 
+const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 800;
+
+
 int main(void)
 {
-	StartGL();
-	UpdateGL();
-	CLOSEWIN();
+	//StartGL();
+	//UpdateGL();
+	//CLOSEWIN();
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);						//needed for MacOS
+	#endif
+
+	
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Ozgun GL", NULL, NULL);
+	if (!window)
+	{
+		LOG("Failed to Create GLFW Window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//Need a wrapper like GLAD, GLEW etc to easily wrap OpenGL functions like glViewport();
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))	//platform specific
+	{
+		LOG("Failed to Initialize GLAD");
+		return -1;
+	}
+	
+	glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+
+	Render(window);
 }
+
+void FrameBufferSizeCallback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, TRUE);
+	}
+}
+
+int Render(GLFWwindow* window)
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		ProcessInput(window);
+
+		glfwSwapBuffers(window);		//swaps buffer for rendering
+		glfwPollEvents();				//for responsiveness
+	}
+	glfwTerminate();
+	return 0;
+}
+
