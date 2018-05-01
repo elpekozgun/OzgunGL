156c91a8fe8e3c0e85143123c487a2f8db8e7b36
Ozgun Elpek <elpekozgun@gmail.com>
2018-05-02 00:42:34 +0300

    VAO implemented

--- OzgunGL/src/MainWindow.cpp
+++ OzgunGL/src/MainWindow.cpp
@@ -2,7 +2,7 @@
 //#include <GL/glew.h>
 //#include <GL/GLU.h>
 #include<glad\glad.h>
-#include <GLFW/glfw3.h>
+#include <GLFW\glfw3.h>
 #include "Macros.h"
 
 void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
@@ -22,8 +22,6 @@ static int StartGL(GLFWwindow* window)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwSetWindowTitle(window, "Celestial Engine V0.0 - Client");
-
-
 
 	if (!window)
 		CLOSEWIN();
@@ -54,7 +52,7 @@ const unsigned int SCR_WIDTH = 800;
 
 const char* vertexShaderSource = 
 "#version 330 core\n"
-"layout(location = 0) in vec3 Pos;\n"
+"layout(location = 15) in vec3 Pos;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(Pos.x, Pos.y, Pos.z, 1.0);\n"
@@ -80,7 +78,7 @@ int main(void)
 	#ifdef __APPLE__
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);						//needed for MacOS
 	#endif
-
+		
 	
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Ozgun GL", NULL, NULL);
 	if (!window)
@@ -90,6 +88,8 @@ int main(void)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
+	
 
 	//Need a wrapper like GLAD, GLEW etc to easily wrap OpenGL functions like glViewport();
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))	//platform specific
@@ -129,27 +129,53 @@ int main(void)
 						 0.5f,-0.5f,0.0f,
 						 0.0f, 0.5f,0.0f};
 
+	float vertices2[] = { -0.5f,-0.5f,0.0f,
+							0.5f, 0.5f,0.0f,
+						   1.0f,-0.0f,0.0f};
+
 	float indices[] = { 0,1,2 };
 
-	unsigned int VAO,VBO,EBO;
-	glGenVertexArrays(1, &VAO);
+	unsigned int VAO,VBO,EBO,VBO2;
+
+#pragma region Binding vertex array using a buffer and unbinding
+
+	glGenVertexArrays(1, &VAO);		// stores the state of a buffer, and can be reused multiple times. A must to use in core profile.
+	glGenBuffers(1, &VBO);			// meaning: Create 1 buffer for me at the memory address of VBO
+
+									// Rule of thumb, generate a vertex array object first before manipulating vertex buffer.
 	glBindVertexArray(VAO);
-
-	glGenBuffers(1,&VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),&vertices[0], GL_STATIC_DRAW);
-
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	//glBindVertexArray(VAO);
-	glEnableVertexAttribArray(0);
-
+	//these 2 functions are the ones that link corresponding VBO to VAO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);													// meaning: Select the vbo buffer to the target which is an array.	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);		// meaning: assign a vertices sized buffer to array buffer target, 
+																						// and assign data starting from vertices[0], and draw them static later.
+																						//glBindBuffer(GL_ARRAY_BUFFER, 0);	if I write it like this, it unselects the array buffer we binded above.
+	glEnableVertexAttribArray(15);														// meaning: Change your state to draw this. Stride = total size of a single vertex attribute (position, normal, color total etc.) YOUR SHADER LOCATION AND THIS MUST BE SAME
+	glVertexAttribPointer(15, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);		// meaning: Layout of the buffer. attribute 0, 3 elements, float type, not normalized, total size of a stride, offset of the attribute
+																						//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);		// offset and position of yout next vertex attribute(eg color)	// layout 15 ten büyükse çizmiyor. 0-15
+																						//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)20);	// offset and position of yout next vertex attribute(eg uv)
+	glBindBuffer(GL_ARRAY_BUFFER, 0);	//after binding the vertex array object with the related vertex buffer, i can simply detach them and call vertex array object whenever i need that.
+	glBindVertexArray(0);
 
 	
 
+#pragma endregion
 
 
-	Render(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ProcessInput(window);
+
+		glBindVertexArray(VAO);		// here we want to draw VAO so we bind the previously generated vertex array object. nice and ez.
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);		//swaps buffer for rendering
+		glfwPollEvents();				//for responsiveness
+	}
+
+	CLOSEWIN();
 }
 
 void FrameBufferSizeCallback(GLFWwindow * window, int width, int height)
@@ -163,25 +189,6 @@ void ProcessInput(GLFWwindow * window)
 	{
 		glfwSetWindowShouldClose(window, TRUE);
 	}
-}
-
-int Render(GLFWwindow* window)
-{
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-		ProcessInput(window);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-		glfwSwapBuffers(window);		//swaps buffer for rendering
-		glfwPollEvents();				//for responsiveness
-	}
-
-	glfwTerminate();
-	return 0;
 }
 
 void ShaderLogger(unsigned int Shader,const char* shaderType)
@@ -211,3 +218,4 @@ void ProgramLinkLogger(unsigned int Program)
 	else
 		LOG("Shader linking Successfull");
 }
+
